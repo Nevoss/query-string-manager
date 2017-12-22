264ccce9b7f824e47a68ce648719611fa077d092
Nevo Golan <nevos12@gmail.com>
2017-12-22 17:33:08 +0200

    plane the testing for the main file

--- test/queryStringManager.spec.js
+++ test/queryStringManager.spec.js
@@ -1,5 +1,56 @@
 import queryStringManager from '../src'
+import expect from 'expect'
 
 describe('queryStringManager', () => {
+
+  it('can parse query string with parser object', () => {
+    
+  })
+
+  it('can strigify queryStringObject with strigifyer object', () => {
+
+  })
+
+  it('added callback function to the listeners array', () => {
+
+  })
+
+  it('call all the listeners array if the item is function', () => {
+
+  })
+
+  it('can set key and value to queryStringObject', () => {
+
+  })
+
+  it('can set an object to queryStringObject', () => {
+
+  })
+
+  it('can get all the queryStringObject', () => {
+
+  })
+
+  it('can get specific key frin queryStringObject', () => {
+
+  })
+
+  it('set a key and push to url at the same method', () => {
+
+  })
+
+  it('can reset the queryStringObject and push to url if wanted', () => {
+
+  })
+
+  it('remove specific from queryStringObject and push to url if wanted', () => {
+
+  })
+
+  it('can read query string parse it and call listeners if wanted', () => {
+
+  })
+
+  // Need to test push to url
 
 })
